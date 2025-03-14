f40df9dda437bf7743d239ee158564079e116263
Ars-BNR <arslan.galimov2018@yandex.ru>
2025-03-14 21:40:04 +0500

    project complete dashboard

---
+++ client/src/app/dashboard/settings/page.tsx
@@ -0,0 +1,11 @@
+import { SettingsForm } from '@/features/user/components'
+import type { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+	title: 'Настройки профиля'
+}
+
+export default function SettingsPage() {
+	return <SettingsForm />
+}
